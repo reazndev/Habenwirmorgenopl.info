a1d8c67072ecbdd82eea4725368157916a153a4f
Reazn <142885331+FlorianRuby@users.noreply.github.com>
2025-03-12 17:56:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Serve static files from the root directory
 app.use(express.static(__dirname));
